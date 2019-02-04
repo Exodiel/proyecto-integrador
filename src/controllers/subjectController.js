a2b65c370fe78782ed501ec4f2e16f3b02c2e539
JIPSON-EXODIEL\jipso <jipson09saad@gmail.com>
2019-02-03 21:52:04 -0500

    ultimos cambios

--- src/controllers/subjectController.js
+++ src/controllers/subjectController.js
@@ -33,7 +33,7 @@ const userScore = async (req, res) => {
 
 const getUserScore = async (req, res) => {
     const {cod_es,cod_mat} = req.body;
-    const result = await pool.query('SELECT * FROM respuestas WHERE cod_es = ? AND cod_mat = 1',[cod_es,cod_mat]);
+    const result = await pool.query('select nom_mat,correcto,incorrecto from respuestas,materia where cod_mat=id_mat and cod_es=? and cod_mat=?;',[cod_es,cod_mat]);
 
     if (result.length > 0) {
         res.status(200).json({
