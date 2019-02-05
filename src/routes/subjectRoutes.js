0eb03997f9773b3bda71df73dc4612697da4689b
JIPSON-EXODIEL\jipso <jipson09saad@gmail.com>
2019-02-04 22:05:18 -0500

    cambios a getUserScore

--- src/routes/subjectRoutes.js
+++ src/routes/subjectRoutes.js
@@ -4,7 +4,7 @@ const subject = require('../controllers/subjectController');
 
 router.get('/subject/:id',subject.subjectUser);
 router.post('/subject',subject.userScore);
-router.get('/subject',subject.getUserScore);
+router.get('/subject/:id/:id',subject.getUserScore);
 router.get('/subject/user/:id',subject.getUserOnly);
 
 module.exports = router;
